5a2594c38765d6f6f70e29e4d6fbbbb8d44a4210
oshou <adf1985adf@gmail.com>
2018-02-07 22:16:36 +0900

    change explanation to English to avoid character corruption

--- setup_iptables.sh
+++ setup_iptables.sh
@@ -1,65 +1,61 @@
 #!/bin/bash
 
 ##################################################################################
-# @@ Define @@
+# @@ Define @@                                                                   #
 ##################################################################################
 
-#--------------------------------------------------------------------------------
-# PATH
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# PATH                                                                           #
+#--------------------------------------------------------------------------------#
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
 
-#--------------------------------------------------------------------------------
-# Allowed IP
-#--------------------------------------------------------------------------------
-DNS_SERVER=(
-  "xxx.xxx.xxx.xxx"
-  "xxx.xxx.xxx.xxx"
-)
-
-NTP_SERVER=(
-  "xxx.xxx.xxx.xxx"
-  "xxx.xxx.xxx.xxx"
-)
-
-PROVISION_SERVER=(
-  "xxx.xxx.xxx.xxx"
-  "xxx.xxx.xxx.xxx"
-)
-
-# 内部ネットワークとして許可する範囲
-# **必要に応じてアンコメントアウト**
+#--------------------------------------------------------------------------------#
+# Allowed IP                                                                     #
+#--------------------------------------------------------------------------------#
 LOCAL_NETS=(
   "xxx.xxx.xxx.xxx/xx"
   "xxx.xxx.xxx.xxx/xx"
   "xxx.xxx.xxx.xxx/xx"
 )
 
-# **必要に応じてアンコメントアウト**
-# * 監視サーバIP等
+# ex) allow ssh access server, monitoring server...
 WHITE_LIST=(
   "xxx.xxx.xxx.xxx/xx"
   "xxx.xxx.xxx.xxx/xx"
   "xxx.xxx.xxx.xxx/xx"
 )
+DNS_SERVER=(
+  "xxx.xxx.xxx.xxx/xx"
+  "xxx.xxx.xxx.xxx/xx"
+)
 
-#--------------------------------------------------------------------------------
-# Denied IP
-#--------------------------------------------------------------------------------
+NTP_SERVER=(
+  "xxx.xxx.xxx.xxx/xx"
+  "xxx.xxx.xxx.xxx/xx"
+)
+
+PROVISION_SERVER=(
+  "xxx.xxx.xxx.xxx/xx"
+  "xxx.xxx.xxx.xxx/xx"
+)
+
+
+#--------------------------------------------------------------------------------#
+# Denied IP                                                                      #
+#--------------------------------------------------------------------------------#
 # if necessary Uncomment out
 #
 # BLACK_LIST=(
-#  "xxx.xxx.xxx.xxx"
-#  "xxx.xxx.xxx.xxx"
-#  "xxx.xxx.xxx.xxx"
+#  "xxx.xxx.xxx.xxx/xx"
+#  "xxx.xxx.xxx.xxx/xx"
+#  "xxx.xxx.xxx.xxx/xx"
 # )
 
 
-
-#--------------------------------------------------------------------------------
-# Port
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Port                                                                           #
+#--------------------------------------------------------------------------------#
 SSH=22
 FTP=20,21
 DNS=53
@@ -74,9 +70,9 @@ NETBIOS=135,137,138,139,445
 DHCP=67,68
 
 
-#--------------------------------------------------------------------------------
-# Function
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Function                                                                       #
+#--------------------------------------------------------------------------------#
 # backup(): backup iptables config
 backup(){
   iptables-save > iptables-`date "+%Y%m%d_%H%M%S"`.bak
@@ -85,12 +81,20 @@ backup(){
 
 # initialize(): initialize policy setting
 initialize(){
-  iptables -P INPUT ACCEPT # 設定のため一時ポリシー変更
-  iptables -P OUTPUT ACCEPT # 設定のため一時ポリシー変更
-  iptables -P FORWARD ACCEPT # 設定のため一時ポリシー変更
-  iptables -F # テーブル初期化
-  iptables -X # チェーン解除
-  iptables -Z # パケットカウンタ・バイトカウンタをゼロリセット
+  # All policy initialize(all access)
+  iptables -P INPUT ACCEPT
+  iptables -P OUTPUT ACCEPT
+  iptables -P FORWARD ACCEPT
+
+  # All table initialize
+  iptables -F
+
+  # All chain initialize
+  iptables -X
+
+  # Reset Transfer Counter(Packet, Byte)
+  iptables -Z
+
   echo 'iptables initialize complete!'
 }
 
@@ -103,77 +107,43 @@ finalize(){
 }
 
 
+
 ##################################################################################
-# @@ Configuration @@
+# @@ Configuration @@                                                            #
 ##################################################################################
-#--------------------------------------------------------------------------------
-# Backup iptables config && Init
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Backup iptables config && Init                                                 #
+#--------------------------------------------------------------------------------#
 backup
 initialize
 
 
-#--------------------------------------------------------------------------------
-# Policy initial setting
-# * INPUT,FORWARD : ALL DROP
-# * OUTPUT        : ALL ACCEPT
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Policy initial setting                                                         #
+# * INPUT,FORWARD : ALL DROP                                                     #
+# * OUTPUT        : ALL ACCEPT                                                   #
+#--------------------------------------------------------------------------------#
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD DROP
 
 
-#--------------------------------------------------------------------------------
-# Allow specific IP/network
-#--------------------------------------------------------------------------------
-
-# ローカルインターフェースの許可
+#--------------------------------------------------------------------------------#
+# Allow specific IP/network                                                      #
+#--------------------------------------------------------------------------------#
+# Local Interface Access
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -i lo -j FORWARD
 
-# セッション確立済パケットの許可
+# Established Access
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# ICMPパケット(ping等)の許可
+# ICMP access
 iptables -A INPUT -p icmp -j ACCEPT
 
-# DNSサーバとの通信許可
-if [ "$DNS_SERVER[@]" != "" ]
-then
-  for dnsserver in ${DNS_SERVER[@]}
-  do
-    iptables -A INPUT -s $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
-    iptables -A FORWARD -s $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
-    iptables -A FORWARD -d $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
-    echo "DNS-SERVER: $dnsserver"
-  done
-fi
 
-# NTPサーバとの通信許可
-if [ "$NTP_SERVER[@]" != "" ]
-then
-  for ntpserver in ${NTP_SERVER[@]}
-  do
-    iptables -A INPUT -s $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
-    iptables -A FORWARD -s $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
-    iptables -A FORWARD -d $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
-    echo "NTP-SERVER: $ntpserver"
-  done
-fi
-
-# プロビジョニングサーバからの通信の許可
-if [ "$PROVISION_SERVER[@]" != "" ]
-then
-  for provisionserver in ${PROVISION_SERVER[@]}
-  do
-    iptables -A INPUT -s $provisionserver -p tcp -m multiport --dports $SSH -j ACCEPT
-    echo "PROVISION-SERVER: $provisionserver"
-  done
-fi
-
-
-# ローカルネットワークの許可
+# Local access (all ports)
 # * SSHアクセスは連続アクセスは10回まで許可、超過した場合はIP毎に平均10秒に1回ペースに制限を行う。
 # * 連続アクセス上限は5分毎に定期的にリセットを行う。
 if [ "$LOCAL_NETS[@]" != "" ]
@@ -192,7 +162,8 @@ then
   done
 fi
 
-# ホワイトリストIP/ネットワーク帯の通信許可
+
+# White List Access(all ports)
 # * SSHアクセスは連続アクセスは10回まで許可、超過した場合はIP毎に平均10秒に1回ペースに制限を行う。
 # * 連続アクセス上限は5分毎に定期的にリセットを行う。
 if [ "$WHITE_LIST[@]" != "" ]
@@ -211,10 +182,45 @@ then
   done
 fi
 
-#--------------------------------------------------------------------------------
-# Allow specific protocol to ALL access
-# ex) http, https...
-#--------------------------------------------------------------------------------
+# DNS access
+if [ "$DNS_SERVER[@]" != "" ]
+then
+  for dnsserver in ${DNS_SERVER[@]}
+  do
+    iptables -A INPUT -s $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
+    iptables -A FORWARD -s $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
+    iptables -A FORWARD -d $dnsserver -p udp -m multiport --dports $DNS -j ACCEPT
+    echo "DNS-SERVER: $dnsserver"
+  done
+fi
+
+# NTP access
+if [ "$NTP_SERVER[@]" != "" ]
+then
+  for ntpserver in ${NTP_SERVER[@]}
+  do
+    iptables -A INPUT -s $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
+    iptables -A FORWARD -s $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
+    iptables -A FORWARD -d $ntpserver -p udp -m multiport --dports $NTP -j ACCEPT
+    echo "NTP-SERVER: $ntpserver"
+  done
+fi
+
+# Provisioning access
+if [ "$PROVISION_SERVER[@]" != "" ]
+then
+  for provisionserver in ${PROVISION_SERVER[@]}
+  do
+    iptables -A INPUT -s $provisionserver -p tcp -m multiport --dports $SSH -j ACCEPT
+    echo "PROVISION-SERVER: $provisionserver"
+  done
+fi
+
+
+#--------------------------------------------------------------------------------#
+# Allow specific protocol to ALL access                                          #
+# ex) http, https...                                                             #
+#--------------------------------------------------------------------------------#
 # For HTTP,HTTPS
 # iptables -A INPUT -p tcp -m multiport --dports $HTTP -j ACCEPT
 
@@ -222,9 +228,9 @@ fi
 # iptables -A INPUT -p tcp -m multiport --dports $FTP -j ACCEPT
 
 
-#--------------------------------------------------------------------------------
-# Deny specific IP/network & deny_logging
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Deny specific IP/network & deny_logging                                        #
+#--------------------------------------------------------------------------------#
 if [ "$BLACK_LIST[@]" != ""  ]
 then
   for black_list_host in ${BLACK_LIST[@]}
@@ -235,11 +241,11 @@ then
 fi
 
 
-#--------------------------------------------------------------------------------
-# Commit
-# * Ctrl+C押下で新iptables設定適用完了
-# * 押さない場合は30秒経過で過去の設定に自動ロールバック
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------#
+# Commit                                                                         #
+# * Ctrl+C              --> itpables config apply                                #
+# * No reaction for 30s --> iptables config rollback                             #
+#--------------------------------------------------------------------------------#
 trap `finalize && exit 0`
 echo "In 30 seconds iptables will be automatically reset."
 echo "Don't forget to test new SSH connection!"
